852b3ced510e90b740cf6d4bade4a9af3e7246d8
Luke Gordon <lgordon@concordservicing.com>
2024-12-06 07:12:48 -0600

    day 6 code improvements/consolidation

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -15,19 +15,22 @@ internal sealed class Solution(char[][] input) : AbstractSolution
 {
     protected override Task<string> SolvePart1Async()
     {
-        var visited = FindGuardPath(input);
-
-        return Task.FromResult(visited.Count.ToString());
+        var evaluation = EvaluateGrid(FindGuard(input), input);
+        
+        if (evaluation.PathResult != PathResult.Exited)
+            throw new InvalidOperationException("Path did not exit the grid");
+        
+        return Task.FromResult(evaluation.Visits.Keys.Count.ToString());
     }
     
     protected override Task<string> SolvePart2Async()
     {
-        var start = FindGuardCoordinates(input);
+        var start = FindGuard(input);
         var height = input.Length;
 
-        var visited = FindGuardPath(input);
+        var visits = EvaluateGrid(start, input).Visits.Keys;
 
-        var obstructions = visited.AsParallel().Count(i =>
+        var obstructions = visits.AsParallel().Count(i =>
         {
             if (i == start)
                 return false;
@@ -39,59 +42,33 @@ internal sealed class Solution(char[][] input) : AbstractSolution
             
             grid[i.Y][i.X] = '#';
 
-            return IsLoop(grid, start);
+            return EvaluateGrid(start, grid).PathResult == PathResult.Loop;
         });
 
         return Task.FromResult(obstructions.ToString());
     }
-
-    private static HashSet<Coordinates> FindGuardPath(char[][] grid)
-    {
-        var coordinates = FindGuardCoordinates(grid);
-        var direction = grid[coordinates.Y][coordinates.X];
-        
-        var width = grid[0].Length;
-        var height = grid.Length;
-
-        var visited = new HashSet<Coordinates> { coordinates };
-
-        while (true)
-        {
-            var next = CalculateNextCoordinates(coordinates, direction);
-            
-            if (next.X < 0 || next.X >= width || next.Y < 0 || next.Y >= height)
-                break;
-            
-            var value = grid[next.Y][next.X];
-
-            if (value == '#')
-                direction = CalculateNextDirection(direction);
-            else
-            {
-                coordinates = next;
-                visited.Add(next);
-            }
-        }
-        
-        return visited;
-    }
     
-    private static bool IsLoop(char[][] grid, Coordinates start)
+    private static GridEvaluation EvaluateGrid(Point start, char[][] grid)
     {
-        var coordinates = start;
-        var direction = grid[coordinates.Y][coordinates.X];
+        var result = new GridEvaluation();
+        
+        var point = start;
+        var direction = grid[point.Y][point.X];
         
         var width = grid[0].Length;
         var height = grid.Length;
 
-        var visited = new Dictionary<Coordinates, List<char>> { { coordinates, [direction] } };
+        result.Visits = new Dictionary<Point, List<char>> { { point, [direction] } };
 
         while (true)
         {
-            var next = CalculateNextCoordinates(coordinates, direction);
+            var next = CalculateNextPoint(point, direction);
 
             if (next.X < 0 || next.X >= width || next.Y < 0 || next.Y >= height)
-                return false;
+            {
+                result.PathResult = PathResult.Exited;
+                break;
+            }
             
             var value = grid[next.Y][next.X];
 
@@ -100,38 +77,25 @@ internal sealed class Solution(char[][] input) : AbstractSolution
                 direction = CalculateNextDirection(direction);
                 continue;
             }
-            if (visited.TryGetValue(next, out var directions))
+            if (result.Visits.TryGetValue(next, out var directions))
             {
                 if (directions.Contains(direction))
-                    return true;
+                {
+                    result.PathResult = PathResult.Loop;
+                    break;
+                }
 
                 directions.Add(direction);
             }
             else            
             {
-                visited.Add(next, [direction]);
+                result.Visits.Add(next, [direction]);
             }
             
-            coordinates = next;
-        }
-    }
-    
-    private static Coordinates FindGuardCoordinates(IReadOnlyList<char[]> grid)
-    {
-        for (var y = 0; y < grid.Count; y++)
-        {
-            for (var x = 0; x < grid[y].Length; x++)
-            {
-                var c = grid[y][x];
-                
-                if (c is Up or Down or Left or Right)
-                {
-                    return new Coordinates(x, y);
-                }
-            }
+            point = next;
         }
 
-        throw new InvalidOperationException("Guard not found");
+        return result;
     }
     
     private const char Up = '^';
@@ -139,7 +103,7 @@ internal sealed class Solution(char[][] input) : AbstractSolution
     private const char Left = '<';
     private const char Right = '>';
 
-    private static Coordinates CalculateNextCoordinates(Coordinates current, char direction)
+    private static Point CalculateNextPoint(Point current, char direction)
     {
         return direction switch
         {
@@ -160,11 +124,33 @@ internal sealed class Solution(char[][] input) : AbstractSolution
             _ => throw new InvalidOperationException("Invalid direction")
         };
     }
+
+    private static Point FindGuard(IReadOnlyList<char[]> grid)
+    {
+        for (var y = 0; y < grid.Count; y++)
+        {
+            for (var x = 0; x < grid[y].Length; x++)
+            {
+                if (grid[y][x] != '.' && grid[y][x] != '#')
+                    return new Point(x, y);
+            }
+        }
+
+        throw new InvalidOperationException("Guard not found");
+    }
 }
 
-internal sealed record Coordinates(int X, int Y);
+internal sealed record Point(int X, int Y);
+
 internal enum PathResult
 {
     Loop,
-    Exited
+    Exited,
+    Unknown
+}
+
+internal class GridEvaluation
+{
+    public PathResult PathResult { get; set; } = PathResult.Unknown;
+    public Dictionary<Point, List<char>> Visits { get; set; } = new();
 }
