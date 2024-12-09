d5fe79403ddeb5c73138eaa47bd6e1d2bc18adfd
Luke Gordon <gordysc@gmail.com>
2024-12-09 17:04:31 -0600

    adding day 10 skeleton project structure

---
+++ Day10/Program.cs
@@ -0,0 +1,21 @@
+﻿using Shared;
+
+var input = File.ReadAllText("Input.txt");
+var solution = new Solution(input);
+
+Console.WriteLine("Day 10");
+
+await solution.SolveAsync();
+
+internal sealed class Solution(string input) : AbstractSolution
+{
+    protected override Task<string> SolvePart1Async()
+    {
+        return Task.FromResult("Not implemented");
+    }
+
+    protected override Task<string> SolvePart2Async()
+    {
+        return Task.FromResult("Not implemented");
+    }
+}
