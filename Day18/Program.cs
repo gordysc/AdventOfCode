13a43fd2d43b59ccf2d1e69f8dfdd663229bb205
Luke Gordon <gordysc@gmail.com>
2024-12-17 08:09:55 -0600

    adding skeleton for day 18

---
+++ Day18/Program.cs
@@ -0,0 +1,22 @@
+﻿using Shared;
+
+var input = File.ReadAllLines("Input.txt");
+
+var solution = new Solution(input);
+
+Console.WriteLine("Day 18");
+
+await solution.SolveAsync();
+
+internal sealed class Solution(string[] input) : AbstractSolution
+{
+    protected override Task<string> SolvePart1Async()
+    {
+        return Task.FromResult("Not implemented");
+    }
+
+    protected override Task<string> SolvePart2Async()
+    {
+        return Task.FromResult("Not implemented");
+    }
+}
