14f946205bdae5fd069e83aa62ae052178c49d91
Luke Gordon <gordysc@gmail.com>
2024-12-20 22:00:09 -0600

    day 1 solution

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -11,11 +11,40 @@ internal sealed class Solution(string[] input) : AbstractSolution
 {
     protected override string SolvePart1()
     {
-        return "Not implemented";
+        var values = input.Select(int.Parse).ToList();
+        
+        for (var i = 0; i < values.Count; i++)
+        {
+            for (var j = i + 1; j < values.Count; j++)
+            {
+                if (values[i] + values[j] == 2020)
+                {
+                    return (values[i] * values[j]).ToString();
+                }
+            }
+        }
+        
+        return "Not found";
     }
     
     protected override string SolvePart2()
     {
+        var values = input.Select(int.Parse).ToList();
+        
+        for (var i = 0; i < values.Count; i++)
+        {
+            for (var j = i + 1; j < values.Count; j++)
+            {
+                for (var k = j + 1; k < values.Count; k++)
+                {
+                    if (values[i] + values[j] + values[k] == 2020)
+                    {
+                        return (values[i] * values[j] * values[k]).ToString();
+                    }
+                }
+            }
+        }
+        
         return "Not implemented";
     }
 }
