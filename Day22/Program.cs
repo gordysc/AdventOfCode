a3e93afe2a673afd4f10245677d377d80d60d4cb
Luke Gordon <gordysc@gmail.com>
2024-12-21 11:11:27 -0600

    add placeholders for day 22

---
+++ Day22/Program.cs
@@ -0,0 +1,23 @@
+﻿using Shared;
+
+var input = File.ReadAllLines("Input.txt");
+
+var solution = new Solution(input);
+
+Console.WriteLine("Day 22");
+
+await solution.SolveAsync();
+
+internal sealed class Solution(string[] input) : AbstractSolution
+{
+    protected override Task<string> SolvePart1Async()
+    {
+        return Task.FromResult("Not implemented");
+    }
+
+    protected override Task<string> SolvePart2Async()
+    {
+        return Task.FromResult("Not implemented");
+    }
+}
+
